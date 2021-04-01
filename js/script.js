2a1971c1d1b5fd7ad30334bc12e0c6371d8144e8
Valentyn <example@gmail.com>
2021-04-01 19:50:39 +0300

    films cycles + conditions

--- js/script.js
+++ js/script.js
@@ -11,11 +11,25 @@ const personalMovieDB = {
     privat: false
 };
 
+for (let i = 0; i < 2; i++) {
+    let film = prompt("Один из последних просмотренных фильмов?");
+    if (film.length < 1 || film.length > 50 || film == null) {
+        i--;
+    }
+    personalMovieDB.movies[film] = +prompt("На сколько оцените его ?");
+}
 
-const film = prompt("Один из последних просмотренных фильмов?");
-personalMovieDB.movies[film] = prompt("На сколько оцените его ?");
-
-const film2 = prompt("Один из последних просмотренных фильмов?");
-personalMovieDB.movies[film2] = prompt("На сколько оцените его ?");
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+}
+else if (personalMovieDB.count > 10 && personalMovieDB.count < 30){
+    alert("Вы классический зритель");
+}
+else if (personalMovieDB.count > 30) {
+    alert("Вы киноман");
+}
+else {
+    alert("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
